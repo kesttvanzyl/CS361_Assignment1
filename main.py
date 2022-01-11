b45a707452e9a54d5371ad44a7de7a8194d75c88
Kestt van Zyl <54337592+kesttvanzyl@users.noreply.github.com>
2022-01-10 21:49:29 -0800

    Test Commit Adding Sample Code

---
+++ main.py
@@ -0,0 +1,10 @@
+# Simple Sample Program to test Github Commit for Assignment 1 in CS 361
+
+x = 5 + 15
+y = 5 * 27
+z = x + y
+
+print("Hello world! My name is Kestt!")
+print("The value of x is: " + format(x))
+print("The value of y is: " + format(y))
+print("The sum of x and y is: " + format(z))
